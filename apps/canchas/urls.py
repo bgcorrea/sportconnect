cd74d8cd448fb3df156ca44e005743170f5c6688
bgcorrea <bgcorrea@uc.cl>
2026-05-04 14:44:27 -0400

    migra urls de url() a path()

--- apps/canchas/urls.py
+++ apps/canchas/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from apps.canchas import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,18 +6,18 @@ from django.conf.urls.static import static
 app_name = 'canchas'
 
 cancha_patterns = [
-    url(r'^inicio/$', views.cancha_list, name='home'),
-    url(r'^(?P<pk>[0-9]+)/$', views.cancha_detail, name='cancha-detail'),
-    url(r'^crear/$', views.cancha_create, name='cancha-create'),
-    url(r'^(?P<pk>[0-9]+)/editar/$', views.cancha_update, name='cancha-edit'),
-    url(r'^(?P<pk>[0-9]+)/eliminar/$', views.cancha_delete, name='cancha-delete'),
+    path('inicio/', views.cancha_list, name='home'),
+    path('<int:pk>/', views.cancha_detail, name='cancha-detail'),
+    path('crear/', views.cancha_create, name='cancha-create'),
+    path('<int:pk>/editar/', views.cancha_update, name='cancha-edit'),
+    path('<int:pk>/eliminar/', views.cancha_delete, name='cancha-delete'),
 ]
 
 urlpatterns = [
-    url(r'^$', views.log_in, name='log-in'),
-    url(r'^log-out/$', views.log_out, name='log-out'),
-    url(r'^deportes/$', views.deporte_list, name='deporte-list'),
-    url(r'^reservas/$', views.reserva_list, name='reserva-list'),
-    url(r'^reservas/crear/$', views.reserva_create, name='reserva-create'),
-    url(r'^canchas/', include(cancha_patterns)),
+    path('', views.log_in, name='log-in'),
+    path('log-out/', views.log_out, name='log-out'),
+    path('deportes/', views.deporte_list, name='deporte-list'),
+    path('reservas/', views.reserva_list, name='reserva-list'),
+    path('reservas/crear/', views.reserva_create, name='reserva-create'),
+    path('canchas/', include(cancha_patterns)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
